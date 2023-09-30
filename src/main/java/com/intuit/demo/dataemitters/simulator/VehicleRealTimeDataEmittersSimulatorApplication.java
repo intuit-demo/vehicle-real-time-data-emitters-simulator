673fa66018e92fa870dd5ea3314484474ea54aca
Sunstreaker <suthagar.pp@gmail.com>
2023-09-30 14:37:47 +0530

    initial commit

---
+++ src/main/java/com/intuit/demo/dataemitters/simulator/VehicleRealTimeDataEmittersSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.intuit.demo.dataemitters.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRealTimeDataEmittersSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRealTimeDataEmittersSimulatorApplication.class, args);
+	}
+
+}
